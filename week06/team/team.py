1ca10bc7557d77d42f68de91c7c7093a0c54de78
samanthastaheli <sta18011@byui.edu>
2022-02-09 17:20:24 -0800

    w6 team

--- week06/team/team.py
+++ week06/team/team.py
@@ -17,13 +17,17 @@ After you can copy a text file word by word exactly
 
 import multiprocessing as mp
 from multiprocessing import Value, Process
-import filecmp 
+import filecmp
+import multiprocessing
+from tracemalloc import start 
 
 # Include cse 251 common Python files
 from cse251 import *
 set_working_directory(__file__)
 
-def sender():
+PIPECOUNT = 0
+
+def sender(conn, filename1):
     """ function to send messages to other end of pipe """
     '''
     open the file
@@ -31,17 +35,37 @@ def sender():
     Note: you must break each line in the file into words and
           send those words through the pipe
     '''
-    pass
+    data = open(filename1, 'r')
 
+    for line in data:
+        # conn.send('New Line')
+        for word in line.split(' '):
+            conn.send(word)
+    conn.send('DONE')
+    conn.close()
+        
 
-def receiver():
+def receiver(conn, filename2, pipe_count):
     """ function to print the messages received from other end of pipe """
     ''' 
     open the file for writing
     receive all content through the shared pipe and write to the file
     Keep track of the number of items sent over the pipe
     '''
-    pass
+    index = 0
+    while True:
+        word = conn.recv()
+
+        with open(filename2, 'a') as f:
+            if word == 'DONE':
+                break
+            elif '\n' in word:
+                f.write(word)
+            else:
+                f.write(word + ' ')
+            index += 1
+            pipe_count.value = index
+    conn.close()
 
 
 def are_files_same(filename1, filename2):
@@ -51,22 +75,29 @@ def are_files_same(filename1, filename2):
 
 def copy_file(log, filename1, filename2):
     # TODO create a pipe 
-    
+    parent_conn, child_conn = mp.Pipe()
+
     # TODO create variable to count items sent over the pipe
+    pipe_count = Value('i', 0)
 
     # TODO create processes 
+    sender_process = multiprocessing.Process(target=sender, args=(parent_conn, filename1))
+    receiver_process = multiprocessing.Process(target=receiver, args=(child_conn, filename2, pipe_count))
 
     log.start_timer()
     start_time = log.get_time()
 
     # TODO start processes 
-    
+    sender_process.start()
+    receiver_process.start()
     # TODO wait for processes to finish
+    sender_process.join()
+    receiver_process.join()
 
     stop_time = log.get_time()
 
-    log.stop_timer(f'Total time to transfer content = {PUT YOUR VARIABLE HERE}: ')
-    log.write(f'items / second = {PUT YOUR VARIABLE HERE / (stop_time - start_time)}')
+    log.stop_timer(f'Total time to transfer content = {pipe_count.value}: ')
+    log.write(f'items / second = {pipe_count.value / (stop_time - start_time)}')
 
     if are_files_same(filename1, filename2):
         log.write(f'{filename1} - Files are the same')
