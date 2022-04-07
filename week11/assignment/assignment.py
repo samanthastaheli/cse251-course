98de193e9e4d90546cb661557e57ee3922a9458c
samanthastaheli <sta18011@byui.edu>
2022-04-07 11:27:53 -0700

    week 11 done

--- week11/assignment/assignment.py
+++ week11/assignment/assignment.py
@@ -4,6 +4,7 @@ Lesson Week: 11
 File: Assignment.py
 """
 
+from multiprocessing.managers import SharedMemoryManager
 import time
 import random
 import multiprocessing as mp
@@ -35,7 +36,7 @@ def guest_partying(id):
     print(f'Guest {id}')
     time.sleep(random.uniform(0, 1))
 
-def cleaner(lock):
+def cleaner(cleaner_lock, room_key, start_time, cleaned_count, cleaner_id):
     """
     do the following for TIME seconds
     cleaner will wait to try to clean the room (cleaner_waiting())
@@ -44,9 +45,26 @@ def cleaner(lock):
         Take some time cleaning (cleaner_cleaning())
         display message STOPPING_CLEANING_MESSAGE
     """
-    pass
+    finish_time = start_time + TIME
 
-def guest(lock):
+    while True:
+        if time.time() > finish_time:
+            return
+
+        cleaner_lock.acquire()
+        room_key.acquire()
+        cleaned_count[0] += 1
+        print(STARTING_CLEANING_MESSAGE)
+        cleaner_cleaning(cleaner_id)
+
+        print(STOPPING_CLEANING_MESSAGE)
+
+        room_key.release()
+        cleaner_lock.release()
+        
+        cleaner_waiting()
+
+def guest(room_key, start_time, party_count, guest_id, room_pop, organizing_lock):
     """
     do the following for TIME seconds
     guest will wait to try to get access to the room (guest_waiting())
@@ -55,24 +73,73 @@ def guest(lock):
         Take some time partying (guest_partying())
         display message STOPPING_PARTY_MESSAGE if the guest is the last one leaving in the room
     """
-    pass
+    finish_time = start_time + TIME
+
+    while True:
+        if time.time() > finish_time:
+            return
+
+        organizing_lock.acquire()
+
+        room_pop[0] += 1
+
+        if room_pop[0] == 1:
+            room_key.acquire()
+            party_count[0] += 1
+            print(STARTING_PARTY_MESSAGE)
+
+        organizing_lock.release()
+        guest_partying(guest_id)
+        organizing_lock.acquire()
+        room_pop[0] -= 1
+
+        if room_pop[0] == 0:
+            room_key.release()
+            print(STOPPING_PARTY_MESSAGE)
+            
+        organizing_lock.release()
+        guest_waiting()
 
 def main():
     # TODO - add any variables, data structures, processes you need
     # TODO - add any arguments to cleaner() and guest() that you need
 
-    cleaned_count = 0 
-    party_count = 0
+    smm = SharedMemoryManager()
+    smm.start()
+    # smm.join()
+
+    cleaned_count = smm.ShareableList([0] * 1)
+    party_count = smm.ShareableList([0] * 1)
+    room_pop = smm.ShareableList([0] * 2) # amount of guest and cleaners in room
 
     # 2 locks required
-    guest_lock = mp.Lock()
     cleaner_lock = mp.Lock()
+    room_key = mp.Lock()
+    organizing_lock = mp.Lock()
 
     # Start time of the running of the program. 
     start_time = time.time()
 
+    cleaner_list = []
+    for i in range(CLEANING_STAFF):
+        cleaner_id = i + 1
+        cleaner_list.append(mp.Process(target=cleaner, args=(cleaner_lock, room_key, start_time, cleaned_count, cleaner_id)))
+        cleaner_list[i].start()
+    
+    guest_list = []
+    for i in range(HOTEL_GUESTS):
+        guest_id = i + 1
+        guest_list.append(mp.Process(target=guest, args=(room_key, start_time, party_count, guest_id, room_pop, organizing_lock)))
+        guest_list[i].start()
+        
+    for i in range(CLEANING_STAFF):
+        cleaner_list[i].join()
+
+    for i in range(HOTEL_GUESTS):
+        guest_list[i].join()
+
     # Results
-    print(f'Room was cleaned {cleaned_count} times, there were {party_count} parties')
+    print(f'Room was cleaned {cleaned_count[0]} times, there were {party_count[0]} parties')
 
 
 if __name__ == '__main__':
